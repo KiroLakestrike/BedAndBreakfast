e5c998dcea4342f9fd18c11e6c91ad9c70e0a7c7
Pascal Klodel <99654847+KiroLakestrike@users.noreply.github.com>
2025-11-09 18:01:11 +0100

    Set up initial project structure with basic web server, routes, templates, and configuration.

---
+++ cmd/web/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/KiroLakestrike/BedAndBreakfast/pkg/config"
+	"github.com/KiroLakestrike/BedAndBreakfast/pkg/handlers"
+	"github.com/KiroLakestrike/BedAndBreakfast/pkg/render"
+)
+
+func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+	app.PortNumber = ":8080"
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	// Load Handlers
+	srv := &http.Server{
+		Addr:    app.PortNumber,
+		Handler: routes(&app),
+	}
+
+	fmt.Println(fmt.Sprintf("Listening on http://localhost%v", app.PortNumber))
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,45 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/KiroLakestrike/BedAndBreakfast/pkg/config"
+	"github.com/KiroLakestrike/BedAndBreakfast/pkg/models"
+	"github.com/KiroLakestrike/BedAndBreakfast/pkg/render"
+)
+
+var Repo *Repository
+
+// Repository type is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+// Home Handler for our Home page
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+}
+
+// About Handler for our About page
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	// Perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again"
+
+	// send the data to the template
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
