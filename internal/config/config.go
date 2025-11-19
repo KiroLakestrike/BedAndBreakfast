9af871363f9b77c929d0ad8120abd96309048588
Pascal Klodel <99654847+KiroLakestrike@users.noreply.github.com>
2025-11-19 16:22:02 +0100

    Reapply "Add form handling, validation, session management, and reservation summary functionality"
    This reverts commit e65a92905eea92c37c4c52e70e835b4af06127a2.

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,10 +1,18 @@
 package config
 
-import "html/template"
+import (
+	"html/template"
+	"log"
+
+	"github.com/alexedwards/scs/v2"
+)
 
 // This is just a basic configuation file
 type AppConfig struct {
 	UseCache      bool
 	TemplateCache map[string]*template.Template
 	PortNumber    string
+	InfoLog       *log.Logger
+	InProduction  bool
+	Session       *scs.SessionManager
 }
